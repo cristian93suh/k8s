db568bb2bd5eb09cdd126c905949246f6ef59333
sudo_Cris <99447855+cristian93suh@users.noreply.github.com>
2024-02-05 18:01:39 +0100

    Add files via upload

---
+++ demo_nodejs_webpage-main/features/step_definitions/loginSteps.js
@@ -0,0 +1,14 @@
+const { Given, When, Then } = require('cucumber');
+
+Given('the user is on the login page', function () {
+  // Implementa il codice relativo a questa dichiarazione
+});
+
+When('the user enters valid credentials', function () {
+  // Implementa il codice relativo a questa dichiarazione
+});
+
+Then('the user should be successfully logged in', function () {
+  // Implementa il codice relativo a questa dichiarazione
+});
+
