5518d28b467f8607d18a3a51d32fa9f61625540d
sudo_Cris <99447855+cristian93suh@users.noreply.github.com>
2024-02-23 17:53:02 +0100

    Update app.js

--- demo_nodejs_webpage-main/app.js
+++ demo_nodejs_webpage-main/app.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var apiMetrics = require('prometheus-api-metrics');
-
-app.use(apiMetrics());
 
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
